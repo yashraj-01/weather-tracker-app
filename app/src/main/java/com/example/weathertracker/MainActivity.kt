0dc197d50884c1ddfbd56543ba635654e3843a79
yashraj-01 <yashrajprime@gmail.com>
2021-09-17 20:21:12 +0530

    created about fragment

--- app/src/main/java/com/example/weathertracker/MainActivity.kt
+++ app/src/main/java/com/example/weathertracker/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_map, R.id.nav_history, R.id.nav_settings), drawerLayout)
+                R.id.nav_map, R.id.nav_history, R.id.nav_settings, R.id.nav_about), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
