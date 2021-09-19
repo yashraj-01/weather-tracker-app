7dc0cafc1ec0c17a5a371830f98b6ac4a4976e23
yashraj-01 <yashrajprime@gmail.com>
2021-09-19 05:31:49 +0530

    added database features

--- app/src/main/java/com/example/weathertracker/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/weathertracker/ui/map/MapFragment.kt
@@ -29,8 +29,11 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import org.json.JSONObject
 import java.util.*
 import kotlin.math.round
@@ -47,6 +50,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var dbHelper: CheckpointDbHelper
     private lateinit var gMap: GoogleMap
+    private var previousMarker: Marker? = null
+    private var currentMarker: Marker? = null
     private lateinit var geocoder: Geocoder
     private lateinit var mLocationCallback: LocationCallback
 
@@ -119,7 +124,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             interval = 5
             fastestInterval = 0
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            maxWaitTime= 100
+            maxWaitTime = 100
         }
         fusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null)
     }
@@ -167,7 +172,8 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val jsonObject = JSONObject(s)
         var temp = jsonObject.getJSONObject("main").getDouble("temp")
         temp = round(temp * 100) / 100
-        val weatherDesc = (jsonObject.getJSONArray("weather")[0] as JSONObject).getString("description")
+        val weatherDesc =
+            (jsonObject.getJSONArray("weather")[0] as JSONObject).getString("description")
         return Pair(temp, weatherDesc)
     }
 
@@ -191,7 +197,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         }
 
         val newRowId = db?.insert(CheckpointEntry.TABLE_NAME, null, values)
-        if(newRowId == -1L) {
+        if (newRowId == -1L) {
             Toast.makeText(
                 activity,
                 "Something went wrong. Checkpoint not added to database.",
@@ -201,18 +207,78 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             Toast.makeText(
                 activity,
                 "Lat/Lng: (${values[CheckpointEntry.COLUMN_NAME_LAT]}, ${values[CheckpointEntry.COLUMN_NAME_LONG]})\n" +
-                "You were here: ${values[CheckpointEntry.COLUMN_NAME_ADDRESS]}" +
-                "Temp: ${"%.2f".format(values[CheckpointEntry.COLUMN_NAME_TEMP])}\u2103 (${values[CheckpointEntry.COLUMN_NAME_WEATHER_DESC]})",
+                        "You were here: ${values[CheckpointEntry.COLUMN_NAME_ADDRESS]}\n" +
+                        "Temp: ${"%.2f".format(values[CheckpointEntry.COLUMN_NAME_TEMP])}\u2103 (${values[CheckpointEntry.COLUMN_NAME_WEATHER_DESC]})",
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    private fun fetchCheckpointsFromDb(): Map<LatLng, List<String>> {
+        val db = dbHelper.readableDatabase
+        val projection = arrayOf(
+            CheckpointEntry.COLUMN_NAME_LAT,
+            CheckpointEntry.COLUMN_NAME_LONG,
+            CheckpointEntry.COLUMN_NAME_ADDRESS,
+            CheckpointEntry.COLUMN_NAME_TEMP,
+            CheckpointEntry.COLUMN_NAME_WEATHER_DESC
+        )
+        val cursor = db.query(
+            CheckpointEntry.TABLE_NAME,   // The table to query
+            projection,             // The array of columns to return (pass null to get all)
+            null,              // The columns for the WHERE clause
+            null,          // The values for the WHERE clause
+            null,                   // don't group the rows
+            null,                   // don't filter by row groups
+            null               // The sort order
+        )
+        val locations = mutableMapOf<LatLng, List<String>>()
+        with(cursor) {
+            while (moveToNext()) {
+                val lat = getDouble(getColumnIndexOrThrow(CheckpointEntry.COLUMN_NAME_LAT))
+                val long = getDouble(getColumnIndexOrThrow(CheckpointEntry.COLUMN_NAME_LONG))
+                val address = getString(getColumnIndexOrThrow(CheckpointEntry.COLUMN_NAME_ADDRESS))
+                val weatherDesc =
+                    getString(getColumnIndexOrThrow(CheckpointEntry.COLUMN_NAME_WEATHER_DESC))
+                val temp =
+                    getDouble(getColumnIndexOrThrow(CheckpointEntry.COLUMN_NAME_TEMP)).toString()
+                locations.putIfAbsent(LatLng(lat, long), listOf<String>(address, temp, weatherDesc))
+            }
+        }
+        cursor.close()
+        return locations
+    }
+
+    private fun deleteCheckpointFromDb(latLng: LatLng) {
+        val db = dbHelper.writableDatabase
+        val lat = latLng.latitude
+        val lng = latLng.longitude
+        val selection =
+            "${CheckpointEntry.COLUMN_NAME_LAT} = $lat AND ${CheckpointEntry.COLUMN_NAME_LONG} = $lng"
+        db.delete(CheckpointEntry.TABLE_NAME, selection, null)
+        Toast.makeText(
+            activity,
+            "Checkpoint deleted.",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         gMap = googleMap
         mLocationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
-                gMap.clear()
+                previousMarker = currentMarker
+                if (previousMarker != null) {
+                    val latlng = previousMarker!!.position
+                    gMap.addMarker(
+                        MarkerOptions().position(latlng)
+                            .title("${latlng.latitude}, ${latlng.longitude}")
+                            .icon(
+                                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+                            )
+                    )
+                }
+                currentMarker?.remove()
                 val locationList = locationResult.locations
                 if (locationList.isNotEmpty()) {
                     val location = locationList.last()
@@ -220,13 +286,39 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                     val lat = location.latitude
                     val long = location.longitude
                     val latLang = LatLng(lat, long)
-                    gMap.addMarker(
+                    currentMarker = gMap.addMarker(
                         MarkerOptions().position(latLang).title("$lat, $long")
                     )
                     gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLang, 15F))
                 }
                 fusedLocationClient.removeLocationUpdates(mLocationCallback)
             }
+        }
+        val locations = fetchCheckpointsFromDb()
+        locations.forEach { location ->
+            gMap.addMarker(
+                MarkerOptions().position(location.key)
+                    .title("${location.key.latitude}, ${location.key.longitude}").icon(
+                        BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+                    )
+            )
+            gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location.key, 15F))
+        }
+        gMap.setOnMarkerClickListener { marker ->
+            if (marker != currentMarker) {
+                val key = marker.position
+                val text =
+                    "Lat/Lng: (${key.latitude}, ${key.longitude})\n" +
+                            "You were here: ${locations[key]?.get(0)}\n" +
+                            "Temp: ${locations[key]?.get(1)}\u2103 (${locations[key]?.get(2)})"
+                Snackbar.make(binding.root, text, Snackbar.LENGTH_SHORT)
+                    .setAction("DELETE") {
+                        deleteCheckpointFromDb(key)
+                        marker.remove()
+                    }
+                    .show()
+                true
+            } else false
         }
     }
 }
